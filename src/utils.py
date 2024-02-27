0643fe69453f4be7ba66e138c1f158bbfa59f3de
rahulr <rahul@active.agency>
2024-02-27 12:18:14 +0530

    added UI

--- src/utils.py
+++ src/utils.py
@@ -20,7 +20,8 @@ class Helpers:
     @staticmethod
     def load_object(file_path):
         try:
-            return pickle.load(file_path)
+            with open(file_path, 'rb') as file_obj:
+                return pickle.load(file_obj)
         except Exception as e:
             raise CustomException(e, sys)
 
